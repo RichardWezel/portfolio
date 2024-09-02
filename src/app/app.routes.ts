937977a20329f0cba7a950ef7f159b15a51d5f68
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-09-02 10:09:47 +0200

    added components to main html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { AboutMeComponent } from './main-content/about-me/about-me.component';
+import { SkillSetComponent } from './main-content/skill-set/skill-set.component';
+import { FeaturedProjectsComponent } from './main-content/featured-projects/featured-projects.component';
 
 export const routes: Routes = [
-  
+   {path: '', component: MainContentComponent},
+   {path: 'imprint', component: ImprintComponent},
+   {path: 'privasy-policy', component: PrivacyPolicyComponent},
+   {path: 'about-me', component: AboutMeComponent},
+   {path: 'skills', component: SkillSetComponent},
+   {path: 'projects', component: FeaturedProjectsComponent},
 ];
