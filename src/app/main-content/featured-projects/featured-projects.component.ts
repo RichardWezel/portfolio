68fe7dca4ba7525920e60766a5007f51c3a883d4
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-09-09 09:37:11 +0200

    hover effect featured projects

--- src/app/main-content/featured-projects/featured-projects.component.ts
+++ src/app/main-content/featured-projects/featured-projects.component.ts
@@ -1,12 +1,32 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-featured-projects',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './featured-projects.component.html',
   styleUrl: './featured-projects.component.scss'
 })
 export class FeaturedProjectsComponent {
 
+ join = {
+    display: 'none',
+  };
+
+  elPolloLoco = {
+    display: 'none',
+  };
+
+  daBubble = {
+    display: 'none',
+  };
+
+  hoverIn(styleObj: any): void {
+    styleObj.display = 'flex';  
+  }
+
+  hoverOut(styleObj: any): void {
+    styleObj.display = 'none'; 
+  }
 }
