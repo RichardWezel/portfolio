c80fad4dd6eb279c49f9e59ab12d74c0a3e458b5
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-10-08 22:13:46 +0200

    links of button ready

--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { InfiniteAutoScrollComponent } from "./infinite-auto-scroll/infinite-auto-scroll.component";
 import { SharedModule } from '../../shared/shared.module';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [NavBarComponent, InfiniteAutoScrollComponent, SharedModule],
+  imports: [NavBarComponent, InfiniteAutoScrollComponent, SharedModule, RouterOutlet],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss', './landing-page.mobile.scss']
 })
--- src/app/main-content/skill-set/skill-set.component.ts
+++ src/app/main-content/skill-set/skill-set.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-skill-set',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule, RouterOutlet],
   templateUrl: './skill-set.component.html',
   styleUrl: './skill-set.component.scss'
 })
