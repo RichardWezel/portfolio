3738cedd019e9d076f04ed93cbf07981939059db
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-10-13 18:02:16 +0200

    fixed button imprint

--- src/app/main-content/imprint/imprint.component.ts
+++ src/app/main-content/imprint/imprint.component.ts
@@ -1,8 +1,9 @@
-// src/app/imprint/imprint.component.ts
+
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ImprintService } from './../../services/imprint.service'; // Passe den Pfad entsprechend an
+import { ImprintService } from './../../services/imprint.service';
 import { Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-imprint',
@@ -12,27 +13,31 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./imprint.component.scss']
 })
 export class ImprintComponent implements OnInit, OnDestroy {
-  isVisible = false; // Interne Variable zur Steuerung der Sichtbarkeit
+  isVisible = false; 
   private subscription!: Subscription;
 
   constructor(private imprintService: ImprintService) {}
 
   ngOnInit() {
-    // Abonniere das Observable, um Änderungen im Sichtbarkeitsstatus zu erhalten
     this.subscription = this.imprintService.isVisible$.subscribe(
       visible => this.isVisible = visible
     );
   }
 
   ngOnDestroy() {
-    // Vermeide Memory Leaks durch das Unsubscriben beim Zerstören der Komponente
     if (this.subscription) {
       this.subscription.unsubscribe();
     }
   }
 
-  // Methode zum Schließen des Imprint
   closeImprint() {
     this.imprintService.hide();
+  }
+
+  @HostListener('document:keydown.escape', ['$event'])
+  onEscape(event: KeyboardEvent) {
+    if (this.isVisible) {
+      this.closeImprint();
+    }
   }
 }
