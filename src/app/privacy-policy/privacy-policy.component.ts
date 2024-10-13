ff1198205913db888bda1f4f69135ea4e7959877
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-10-13 18:42:18 +0200

    added privicy policy

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ImprintService } from './../services/imprint.service';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
-export class PrivacyPolicyComponent {
+export class PrivacyPolicyComponent implements OnInit, OnDestroy{
+  isVisible = false; 
+  private subscription!: Subscription;
 
+  constructor(private imprintService: ImprintService) {}
+
+  ngOnInit() {
+    this.subscription = this.imprintService.isVisible$.subscribe(
+      visible => this.isVisible = visible
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  closePrivacyPolicy() {
+    this.imprintService.hide();
+  }
+
+  @HostListener('document:keydown.escape', ['$event'])
+  onEscape(event: KeyboardEvent) {
+    if (this.isVisible) {
+      this.closePrivacyPolicy();
+    }
+  }
 }
