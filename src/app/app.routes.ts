b7a38b9f15e0f747c7e99c0a68119d6ececdd7bb
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-10-21 08:55:15 +0200

    deleted about-me, skills and projects from app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,11 @@ import { NgModule } from '@angular/core';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ImprintComponent } from './main-content/imprint/imprint.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
-import { AboutMeComponent } from './main-content/about-me/about-me.component';
-import { SkillSetComponent } from './main-content/skill-set/skill-set.component';
-import { FeaturedProjectsComponent } from './main-content/featured-projects/featured-projects.component';
 
 export const routes: Routes = [
    {path: '', component: MainContentComponent},
    {path: 'imprint', component: ImprintComponent},
    {path: 'privasy-policy', component: PrivacyPolicyComponent},
-   {path: 'about-me', component: AboutMeComponent},
-   {path: 'skills', component: SkillSetComponent},
-   {path: 'projects', component: FeaturedProjectsComponent},
 ];
 
 const routerOptions: ExtraOptions = {
