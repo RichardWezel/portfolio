568b9534e4a0a4101241b654e0d883ab652fc4de
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-10-21 08:45:46 +0200

    added nav-bar and footer to app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { SharedModule } from './shared/shared.module';
+import { NavBarComponent } from "./main-content/landing-page/nav-bar/nav-bar.component";
+import { FooterComponent } from "./main-content/footer/footer.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SharedModule],
+  imports: [CommonModule, RouterOutlet, SharedModule, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
