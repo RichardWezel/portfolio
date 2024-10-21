5bbd6c3f8bcbf3223533ce4f70554d3da808f4b1
RichyClean <149680604+RichyClean@users.noreply.github.com>
2024-10-21 08:58:17 +0200

    deleted imprint.service and changed in footer click to routerLink

--- src/app/main-content/footer/footer.component.ts
+++ src/app/main-content/footer/footer.component.ts
@@ -1,6 +1,5 @@
 import { Component} from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
-import { ImprintService } from './../../services/imprint.service'
 
 @Component({
   selector: 'app-footer',
@@ -9,12 +8,4 @@ import { ImprintService } from './../../services/imprint.service'
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss', './footer.mobile.scss']
 })
-export class FooterComponent {
-
-  constructor(private imprintService: ImprintService) {}
-
-  openImprint(event: Event) {
-    event.preventDefault(); 
-    this.imprintService.show();
-  }
-}
+export class FooterComponent {}
